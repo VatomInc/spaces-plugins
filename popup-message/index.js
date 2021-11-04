c62716ee0e134aba828dc64d3e31dd15da3ffcbd
Kyle Chapman <kyleichapman@gmail.com>
2021-11-04 09:00:02 +0200

    Remove trailing comma

--- popup-message/index.js
+++ popup-message/index.js
@@ -29,7 +29,7 @@ module.exports = class PopupMessage extends BasePlugin {
                     { type: 'checkbox', id: 'enabled', name: 'Enabled', help: 'When enabled, the popup will be shown.' },
                     { type: 'text', id: 'title', name: 'Title', help: 'Title of the message.' },
                     { type: 'text', id: 'text', name: 'Text', help: 'Text to display in the message.' },
-                    { type: 'select', id: 'icon', name: 'Icon', values: ['', 'info', 'warning', 'question', 'error'] },
+                    { type: 'select', id: 'icon', name: 'Icon', values: ['', 'info', 'warning', 'question', 'error'] }
                 ]
             }
         })
