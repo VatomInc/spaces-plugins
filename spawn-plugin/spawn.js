c57b6133fd05b5fc1e5f366cb6b7503f3834ee23
Zongo Maqutu <MQTZON001@myuct.ac.za>
2022-03-10 11:29:01 +0200

    added component to perform spam on avatar

--- spawn-plugin/spawn.js
+++ spawn-plugin/spawn.js
@@ -2,11 +2,11 @@
  * Spawn Plugin
  *
  * Spawns a user's avatar on a random place in the world.
+ * Listens for a hook called 'spawn' and moves the user's avatar to a random position .
  *
  * @license MIT
  * @author zmaqutu
  */
-// myplugin.js
 
 module.exports = class SpawnPlugin extends BasePlugin {
 
@@ -18,7 +18,35 @@ module.exports = class SpawnPlugin extends BasePlugin {
         /** Called when the plugin is loaded */
         onLoad() {
     
-            this.menus.alert('Hello World!')
+                this.menus.alert('Hello World!')
+    
+        }
+        onUnload() {
+                        
+                this.menus.alert('Goodbye World!')
+        
+        }
+        onSettingsUpdated(field,) {
+    
+                this.menus.alert('Settings updated!')
+    
+        }
+    
+}
+class SpawnComponent extends BaseComponent {
+
+        /** Called when an object with this component is loaded */
+        onLoad() {
+    
+            console.log('Loaded component!')
+    
+        }
+    
+        /** Called when the user clicks on this object */
+        onClick() {
+    
+            // Show alert
+            this.plugin.menus.alert('Object clicked!')
     
         }
     
