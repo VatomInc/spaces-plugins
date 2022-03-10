58ba6654eb73d013e633af8c3ecbd5ae29026d8a
Zongo Maqutu <MQTZON001@myuct.ac.za>
2022-03-10 14:01:15 +0200

    added test for the spawn plugin

---
+++ spawn-plugin/testSpawn.js
@@ -0,0 +1,17 @@
+// this is a plugin to test the spawn plugin
+module.exports = class TestSpawnPlugin extends BasePlugin {
+
+        /** Plugin info */
+        static get id()             { return 'spawn-test' }
+        static get name()           { return 'Spawn Test Plugin' }
+        static get description()    { return 'A plugin to test the Spawn plugin.' }
+    
+        /** Called when the plugin is loaded */
+        onLoad() {
+    
+            this.menus.alert('Hello World!')
+            this.plugin.hooks.trigger('user-died')
+    
+        }
+    
+}
